97bc4502868cbdf277c985ea383663a7c0d95993
Shashidhar R Bhat <shashidharrbhat@Shashidhars-MacBook-Air.local>
2025-11-30 13:08:58 +0530

    Remove docusaraus, add gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/shashidharrbhat/Documents/workspace/tech-notebook/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/shashidharrbhat/Documents/workspace/tech-notebook/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/shashidharrbhat/Documents/workspace/tech-notebook/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/shashidharrbhat/Documents/workspace/tech-notebook/src/css/custom.css'),
-];
