97397684eb1dd16cb14ca374bf54f0609f173b38
prajwalhaniya <prajwalhaniya@gmail.com>
2025-12-04 16:43:08 +0530

    ignore .docusaurus dir

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/prajwal/Documents/workspace/github/tech-notebook/node_modules/infima/dist/css/default/default.css'),
-  require('/home/prajwal/Documents/workspace/github/tech-notebook/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/prajwal/Documents/workspace/github/tech-notebook/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/prajwal/Documents/workspace/github/tech-notebook/src/css/custom.css'),
-];
